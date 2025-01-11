8e9e42fe4fe73422dda6d72263c912f731ac3a01
joegenequ <joegenequesada@gmail.com>
2025-01-11 13:14:10 +0800

    update

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,3 +1,20 @@
+<script>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
+
+const countries = ref([])
+
+async function getCountries() {
+  const { data } = await supabase.from('countries').select()
+  countries.value = data
+}
+
+onMounted(() => {
+  getCountries()
+})
+
+</script>
+
 <template>
   <h1>Food</h1>
   <food-item/>
