b7e4e357c7b62ed6535e602b2a7b0a8a06507ba8
joegenequ <joegenequesada@gmail.com>
2025-01-11 04:26:41 +0000

    supabaes quickstart

---
+++ firstsfc/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://fhcvnsawkldodwgkqbnl.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoY3Zuc2F3a2xkb2R3Z2txYm5sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY1NjkwNDQsImV4cCI6MjA1MjE0NTA0NH0.hJdhL0LWHK_fWhOS-5Xb_4UGvy7idbhs2dCdouYH9sU')
