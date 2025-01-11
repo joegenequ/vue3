d5260d397193641c1e8f50b6fe46502dca2be5e4
joegenequ <joegenequesada@gmail.com>
2025-01-11 13:05:26 +0800

    update

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,20 +1,3 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
-
-const countries = ref([])
-
-async function getCountries() {
-  const { data } = await supabase.from('countries').select()
-  countries.value = data
-}
-
-onMounted(() => {
-  getCountries()
-})
-</script>
-
-
 <template>
   <h1>Food</h1>
   <food-item/>
--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -2,6 +2,24 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
 
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
+
+const countries = ref([])
+
+async function getCountries() {
+  const { data } = await supabase.from('countries').select()
+  countries.value = data
+}
+
+onMounted(() => {
+  getCountries()
+})
+
+
+
 const app = createApp(App)
 app.component('food-item', FoodItem)
 app.mount('#app')
+
+
