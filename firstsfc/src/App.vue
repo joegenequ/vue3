4f3cdcfe497f96c991d1f3f08f262e4742cc35fe
joegenequ <joegenequesada@gmail.com>
2025-01-11 04:44:15 +0000

    added food-item

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -19,3 +19,24 @@ onMounted(() => {
     <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
   </ul>
 </template>
+
+
+
+<template>
+  <h1>Food</h1>
+  <food-item/>
+  <food-item/>
+  <food-item/>
+</template>
+
+<script></script>
+
+<style>
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightyellow;
+  }
+</style>
