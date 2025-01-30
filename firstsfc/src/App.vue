6da8042100d9fe720f35aa78092337e9f8b2690d
joegenequ <joegenequesada@gmail.com>
2025-01-31 01:53:35 +0800

    list comments added

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -5,6 +5,7 @@
   <food-item/>
   <food-item/>
   <comment-form/>
+  <comment/>
    
 
 </template>
--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
 import Country from './components/Country.vue'
 import CommentForm from './components/CommentForm.vue'
+import Comment from './components/Comment.vue'
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
 app.component('country', Country)
 app.component('comment-form', CommentForm)
+app.component('comment', Comment)
 app.mount('#app')
 
 
