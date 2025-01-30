fb4b60dbb58d48614665ace0ddab0037a2d3a258
joegenequ <joegenequesada@gmail.com>
2025-01-30 18:37:26 +0800

    moved country to component

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
+import Country from './components/Country.vue'
 
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
+app.component('country', Country)
 app.mount('#app')
 
 
