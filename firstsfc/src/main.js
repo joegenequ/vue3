3a122d608a407962ce9903ccebf3943ce0cb49ac
joegenequ <joegenequesada@gmail.com>
2025-02-10 05:19:53 +0000

    update file name

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -4,7 +4,7 @@ import FoodItem from './components/FoodItem.vue'
 import Country from './components/Country.vue'
 import CommentForm from './components/CommentForm.vue'
 import Comment from './components/Comment.vue'
-import RestApi from './components/restApi.vue'
+import RestApi from './components/RestApi.vue'
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
