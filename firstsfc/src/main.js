761d576e4fdd7ace0792491ca19a16a968d9e011
joegenequ <joegenequesada@gmail.com>
2025-02-10 05:13:59 +0000

    Rest API component

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -4,12 +4,14 @@ import FoodItem from './components/FoodItem.vue'
 import Country from './components/Country.vue'
 import CommentForm from './components/CommentForm.vue'
 import Comment from './components/Comment.vue'
+import RestApi from './components/restApi.vue'
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
 app.component('country', Country)
 app.component('comment-form', CommentForm)
 app.component('comment', Comment)
+app.component('rest-api', RestApi)
 app.mount('#app')
 
 
