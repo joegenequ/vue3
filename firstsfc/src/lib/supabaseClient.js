74af41c7ecd46b605f6e1371e3f3cb26c6788c74
joegenequ <joegenequesada@gmail.com>
2025-09-25 11:46:28 +0800

    Update supabaseClient.js

--- firstsfc/src/lib/supabaseClient.js
+++ firstsfc/src/lib/supabaseClient.js
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://fhcvnsawkldodwgkqbnl.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZoY3Zuc2F3a2xkb2R3Z2txYm5sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY1NjkwNDQsImV4cCI6MjA1MjE0NTA0NH0.hJdhL0LWHK_fWhOS-5Xb_4UGvy7idbhs2dCdouYH9sU')
+export const supabase = createClient('https://qxecahopjwhrlcintptl.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4ZWNhaG9wandocmxjaW50cHRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3NzAxODMsImV4cCI6MjA3NDM0NjE4M30.h97RsIt1bUvbBPHTANE-MJj6KnEOoO3qGtyxJSk4Vzw')
