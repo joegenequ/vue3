6c25d749efa701ae972d8896acc0cb82c3f822df
joegenequ <joegenequesada@gmail.com>
2025-09-25 11:51:45 +0800

    Update supabaseClient.js

--- firstsfc/src/lib/supabaseClient.js
+++ firstsfc/src/lib/supabaseClient.js
@@ -1,3 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://qxecahopjwhrlcintptl.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4ZWNhaG9wandocmxjaW50cHRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3NzAxODMsImV4cCI6MjA3NDM0NjE4M30.h97RsIt1bUvbBPHTANE-MJj6KnEOoO3qGtyxJSk4Vzw')
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_K
+
+export const supabase = createClient(supabaseUrl, supabasePublishableKey)
